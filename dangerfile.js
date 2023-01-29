c7ee3b684825b1f5d3e989ee9df28e23ff1f35cc
Gustavo Figueiredo <gustavodsf1@gmail.com>
2023-01-28 22:23:11 -0300

    add danger.js

--- dangerfile.js
+++ dangerfile.js
@@ -1,19 +1,7 @@
-// dangerfile.js
-import {danger, warn} from "danger"
-
-
-const changes = danger.git.modified_files.reduce((prev, filePath) => {
-  if(!prev.package) {
-    prev.package = filePath.includes("package.json");
-  }
-  if(!prev.lock) {
-    prev.lock = filePath.includes("package-lock.json")
-  }
-  return prev;
-}, {});
-
-if (changes.package && !changes.lock) {
-  const message = 'Changes were made to package.json, but not to package-lock.json';
-  const idea = 'Perhaps you need to run `npm install`?';
-  warn(`${message} - <i>${idea}</i>`);
+const hasChangelog = danger.git.modified_files.includes("changelog.md");
+const isTrivial = (danger.github.pr.body + danger.github.pr.title).includes(
+  "#trivial"
+);
+if (!hasChangelog && !isTrivial) {
+  warn("Please add a changelog entry for your changes.");
 }
