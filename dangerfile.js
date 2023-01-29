2a4a60080b78996c6f0befc8af598906f59d6330
Gustavo Figueiredo <gustavodsf1@gmail.com>
2023-01-29 16:26:01 -0300

    work message

--- dangerfile.js
+++ dangerfile.js
@@ -1,26 +1,27 @@
 import {message, danger, fail, warn} from "danger"
 
+// Check if some file is added.
 const newFiles = danger.git.created_files.join("- ")
 message("New Files in this PR: \n - " + newFiles);
 
 
+// Check if JIRA card number is placed in the title
 const isNoCARD = danger.github.pr.title.includes("[NO-CARD]")
 const isGPV = danger.github.pr.title.includes("[GPV-")
 
 if (!isGPV && !isNoCARD){
-  fail(':exclamation: No Jira Card');
-  markdown(">  We can't see the jira card number on your PR title **(GPV-####).**");
+  fail(':exclamation: No Jira Card -  We can\'t see the jira card number on your PR title (GPV-####).');
 } else if(!isGPV && isNoCARD){
-  warn(':exclamation: NO-CARD');
-  markdown("> We inform you that this change does not have an associated card, correct?");
+  warn(':exclamation: NO-CARD - this change does not have an associated card, correct?');
 }
 
 
+// Check the number of lines changed.
 const WARN_PR_THRESHOLD = 2;
 const FAIL_PR_THRESHOLD = 10;
 
 const linesCount = danger.git.linesOfCode("**/*");
-const excludeLinesCount = danger.git.linesOfCode("**/*mock*");
+const excludeLinesCount = danger.git.linesOfCode("**/*test*");
 const totalLinesCount = linesCount - excludeLinesCount;
 
 if (totalLinesCount > WARN_PR_THRESHOLD  &&  totalLinesCount < FAIL_PR_THRESHOLD) {
@@ -29,3 +30,4 @@ if (totalLinesCount > WARN_PR_THRESHOLD  &&  totalLinesCount < FAIL_PR_THRESHOLD
   fail("Big PR, break down into smaller PRs.");
 }
 
+message(`Total the lines changed: ${totalLinesCount}`);
