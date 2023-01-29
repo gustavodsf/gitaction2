573023ea4993c1e890cb32e4e8f5c7c3f38bff70
Gustavo Figueiredo <gustavodsf1@gmail.com>
2023-01-29 20:28:05 -0300

    add danger spell check

--- dangerfile.js
+++ dangerfile.js
@@ -38,6 +38,39 @@ const hasChangedInTheMessage = () => {
   }
 }
 
+const checkPRChanges = () => {
+  const BIG_PR_THRESHOLD = 10;
+  const MEDIUM_PR_THRESHOLD = 20;
+
+  const lineAdded = danger.github.pr.additions;
+  const lineRemoved = danger.github.pr.deletions;
+  const totalChanges = lineAdded + lineRemoved;
+
+  const method = undefined
+
+  if ( totalChanges  > MEDIUM_PR_THRESHOLD && totalChanges < BIG_PR_THRESHOLD) {
+    method = warn;
+  } else if (totalChanges > BIG_PR_THRESHOLD) {
+    method = fail;
+  }
+
+  if (method) {
+    method(':exclamation: Big PR (' + totalChanges + ')');
+    markdown('> (' + totalChanges + ') : Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.');
+  }
+
+}
+
+const checkPkgLockUpdate = () => {
+  const packageChanged = danger.git.modified_files.includes('package.json');
+  const lockfileChanged = danger.git.modified_files.includes('package-lock.json');
+  if (packageChanged && !lockfileChanged) {
+      warn(`Changes were made to package.json, but not to package-lock.json - <i>'Perhaps you need to run `npm install`?'</i>`);
+  }
+}
+
+checkPRChanges();
+checkPkgLockUpdate();
 checkReview();
 addNewFile();
 hasJiraNumberOnTitle();
