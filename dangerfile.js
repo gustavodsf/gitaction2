ceae4c01b0f66df8538d68cfb53211f635f2e055
Gustavo Figueiredo <gustavodsf1@gmail.com>
2023-01-29 17:07:27 -0300

    add danger

--- dangerfile.js
+++ dangerfile.js
@@ -1,28 +1,34 @@
 import {message, danger, fail, warn} from "danger"
 
-// Check if some file is added.
 
-const newFiles = danger.git.created_files.join("- ")
-message("New Files in this PR: \n - " + newFiles);
+const checkReview = () => {
+  if (danger.github.pr.reviewers === undefined) {
+    fail(':x: Please assign someone to merge this PR.')
+  }
+}
 
+const addNewFile = () => {
+  const newFiles = danger.git.created_files.join("- ")
+  message("New Files in this PR: \n - " + newFiles);
+}
 
 // Check if JIRA card number is placed in the title
-const isNoCARD = danger.github.pr.title.includes("[NO-CARD]")
-const isGPV = danger.github.pr.title.includes("[GPV-")
-
-if (!isGPV && !isNoCARD){
-  fail(':x: No Jira Card -  We can\'t see the jira card number on your PR title (GPV-####).');
-} else if(!isGPV && isNoCARD){
-  warn(':exclamation: NO-CARD - this change does not have an associated card, correct?');
+const hasJiraNumberOnTitle = () => {
+  const isNoCARD = danger.github.pr.title.includes("[NO-CARD]")
+  const isGPV = danger.github.pr.title.includes("[GPV-")
+  
+  if (!isGPV && !isNoCARD){
+    fail(':x: No Jira Card -  We can\'t see the jira card number on your PR title (GPV-####).');
+  } else if(!isGPV && isNoCARD){
+    warn(':exclamation: NO-CARD - this change does not have an associated card, correct?');
+  }
+  
+  // have to add assignee
+  if (danger.github.pr.assignee === null) {
+    warn(':exclamation: Please assign the PR to someone')
+  } 
 }
 
-// have to add assignee
-if (danger.github.pr.assignee === null) {
-  warn(':exclamation: Please assign the PR to someone')
-}
-
-
-message(`Review: ${danger.github.pr.reviewers}`)
-if (danger.github.pr.reviewers === null) {
-  fail(':x: Please assign someone to merge this PR.')
-}
+checkReview();
+addNewFile();
+hasJiraNumberOnTitle();
